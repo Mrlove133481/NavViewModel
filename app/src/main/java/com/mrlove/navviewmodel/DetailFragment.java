3f840e4720f10c9b4b4ff74f5177fabe8945e0e0
Mrlove133481 <1334819077@qq.com>
2020-01-22 14:23:31 +0800

    精简代码

--- app/src/main/java/com/mrlove/navviewmodel/DetailFragment.java
+++ app/src/main/java/com/mrlove/navviewmodel/DetailFragment.java
@@ -1,19 +1,15 @@
 package com.mrlove.navviewmodel;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.SeekBar;
 
 import com.mrlove.navviewmodel.databinding.FragmentDetailBinding;
 
